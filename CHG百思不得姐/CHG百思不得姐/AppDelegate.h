a9215d19afd8b17de28833aaf5ac609593f65d9c
chenhongen <chg@chenhongen.local>
2015-09-01 13:05:45 +0800

    项目基本设置

---
+++ CHG百思不得姐/CHG百思不得姐/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CHG百思不得姐
+//
+//  Created by chenhongen on 15/9/1.
+//  Copyright (c) 2015年 陈弘根. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
